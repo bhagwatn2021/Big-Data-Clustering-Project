97b6181423f297a15822d84159f83e943f08f4e7
Eptisam Kassim <eptisam@Eptisams-MacBook-Pro.local>
2020-03-30 22:27:21 -0400

    q3 complete

--- project2script.r
+++ project2script.r
@@ -2,11 +2,14 @@
 
 #set up the data: read the data into a table, set the variable names and assign them to the data
 install.packages("gmodels")
+install.packages("factoextra")
 install.packages("class")
-library(class)     
+install.packages("psych")
+library(class)
+library(psych)
 
 adults= read.csv("data/adult.data", header=F)
-# Done this way to remove comma
+# Done this way to remove commas
 
 adultnames <- c("age", "workclass", "fnlwgt", "education", "education-num", "marital-status", "occupation","relationship","race","sex","capital-gain","capital-loss","hours-per-week","native-country","income")
 length(adultnames)
@@ -60,14 +63,156 @@ adultsCopy$sex<-as.numeric(adultsCopy$sex)
 adultsCopy <- adultsCopy[-c(11)]
 str(adultsCopy)
 
-
 adultsCopy<-na.omit(adultsCopy)
-#omit the NA
+#omit the NA except does nothing cause can't change ? --> NA
+
+describe(adultsCopy)
+#plot(adultsCopy)
+adultsTest<- adultsCopy[1:398,]
+adultsTest
+str(adultsTest)
+#plot(adultsTest)
+#Suppose to keep the linear or near linear relationships but I can't tell with the plot
+#delete attributes that are not linear 
+
+#normalization Process
+normalize<- function(x) {((x-min(x))/(max(x)-min(x)))}
+normalize
+normalize(c(1,2,3,4,5,6,7,8,9, 10))
+adultsCopy.norm<- as.data.frame(lapply(adultsCopy[,1:10],normalize))
+adultsCopy.norm[1:10,]
+
+#Kmeans 2 to 10
+#It doens't look like the profs example at all
+adultsCopy.k2<-kmeans(adultsCopy.norm, centers = 2, nstart = 25)
+adultsCopy.k2
+factoextra::fviz_cluster(adultsCopy.k2, adultsCopy)
+
+adultsCopy.k3<-kmeans(adultsCopy.norm, centers = 3, nstart = 25)
+adultsCopy.k3
+factoextra::fviz_cluster(adultsCopy.k3, adultsCopy)
+
+adultsCopy.k4<-kmeans(adultsCopy.norm, centers = 4, nstart = 25)
+adultsCopy.k4
+factoextra::fviz_cluster(adultsCopy.k4, adultsCopy)
+
+adultsCopy.k5<-kmeans(adultsCopy.norm, centers = 5, nstart = 25)
+adultsCopy.k5
+factoextra::fviz_cluster(adultsCopy.k5, adultsCopy)
+
+adultsCopy.k6<-kmeans(adultsCopy.norm, centers = 6, nstart = 25)
+adultsCopy.k6
+factoextra::fviz_cluster(adultsCopy.k6, adultsCopy)
+
+adultsCopy.k7<-kmeans(adultsCopy.norm, centers = 7, nstart = 25)
+adultsCopy.k7
+factoextra::fviz_cluster(adultsCopy.k7, adultsCopy)
+
+adultsCopy.k8<-kmeans(adultsCopy.norm, centers = 8, nstart = 25)
+adultsCopy.k8
+factoextra::fviz_cluster(adultsCopy.k8, adultsCopy)
+
+adultsCopy.k9<-kmeans(adultsCopy.norm, centers = 9, nstart = 25)
+adultsCopy.k9
+factoextra::fviz_cluster(adultsCopy.k9, adultsCopy)
+
+adultsCopy.k10<-kmeans(adultsCopy.norm, centers = 10, nstart = 25)
+adultsCopy.k10
+factoextra::fviz_cluster(adultsCopy.k10, adultsCopy)
+
+#shows the optimal number of clusters
+#takes too long on my computer so I couldn't see the results
+# Also we don;t have to do this 
+factoextra::fviz_nbclust(adultsCopy, FUNcluster=kmeans,print.summary=TRUE)
+
+#Prep for knn
+adultsCopy.norm.nrows<-nrow(adultsCopy.norm)
+adultsCopy.norm.sample<-0.7
+adultsCopy.norm.train.index<-sample(adultsCopy.norm.nrows, adultsCopy.norm.sample*adultsCopy.norm.nrows)
+length(adultsCopy.norm.train.index)
+adultsCopy.norm.train<-adultsCopy.norm[adultsCopy.norm.train.index,]
+adultsCopy.norm.train[1:20,]
+adultsCopy.norm.test<-adultsCopy.norm[-adultsCopy.norm.train.index,]
+adultsCopy.norm.test[1:20,]
+
+#knn=2
+adultsCopy.norm.train.k2<-kmeans(adultsCopy.norm.train, centers=2)
+adultsCopy.norm.train.k2
+adultsCopy.norm.test.k2<-knn(adultsCopy.norm.train, adultsCopy.norm.test, adultsCopy.norm.train.k2$cluster, k=2)
+adultsCopy.norm.test.k2
+adultsCopy.norm.train.labels<-adultsCopy.norm.train.k2$cluster
+adultsCopy.norm.train.labels
+
+#knn=3
+adultsCopy.norm.train.k3<-kmeans(adultsCopy.norm.train, centers=3)
+adultsCopy.norm.train.k3
+adultsCopy.norm.test.k3<-knn(adultsCopy.norm.train, adultsCopy.norm.test, adultsCopy.norm.train.k3$cluster, k=3)
+adultsCopy.norm.test.k3
+adultsCopy.norm.train.labels<-adultsCopy.norm.train.k3$cluster
+adultsCopy.norm.train.labels
+
+#knn=4
+adultsCopy.norm.train.k4<-kmeans(adultsCopy.norm.train, centers=4)
+adultsCopy.norm.train.k4
+adultsCopy.norm.test.k4<-knn(adultsCopy.norm.train, adultsCopy.norm.test, adultsCopy.norm.train.k4$cluster, k=4)
+adultsCopy.norm.test.k4
+adultsCopy.norm.train.labels<-adultsCopy.norm.train.k4$cluster
+adultsCopy.norm.train.labels
+
+#knn=5
+adultsCopy.norm.train.k5<-kmeans(adultsCopy.norm.train, centers=5)
+adultsCopy.norm.train.k5
+adultsCopy.norm.test.k5<-knn(adultsCopy.norm.train, adultsCopy.norm.test, adultsCopy.norm.train.k5$cluster, k=5)
+adultsCopy.norm.test.k5
+adultsCopy.norm.train.labels<-adultsCopy.norm.train.k5$cluster
+adultsCopy.norm.train.labels
+
+#knn=6
+adultsCopy.norm.train.k6<-kmeans(adultsCopy.norm.train, centers=6)
+adultsCopy.norm.train.k6
+adultsCopy.norm.test.k6<-knn(adultsCopy.norm.train, adultsCopy.norm.test, adultsCopy.norm.train.k6$cluster, k=6)
+adultsCopy.norm.test.k6
+adultsCopy.norm.train.labels<-adultsCopy.norm.train.k6$cluster
+adultsCopy.norm.train.labels
+
+#knn=7
+adultsCopy.norm.train.k7<-kmeans(adultsCopy.norm.train, centers=7)
+adultsCopy.norm.train.k7
+adultsCopy.norm.test.k7<-knn(adultsCopy.norm.train, adultsCopy.norm.test, adultsCopy.norm.train.k7$cluster, k=7)
+adultsCopy.norm.test.k7
+adultsCopy.norm.train.labels<-adultsCopy.norm.train.k7$cluster
+adultsCopy.norm.train.labels
+
+#knn=8
+adultsCopy.norm.train.k8<-kmeans(adultsCopy.norm.train, centers=8)
+adultsCopy.norm.train.k8
+adultsCopy.norm.test.k8<-knn(adultsCopy.norm.train, adultsCopy.norm.test, adultsCopy.norm.train.k8$cluster, k=8)
+adultsCopy.norm.test.k8
+adultsCopy.norm.train.labels<-adultsCopy.norm.train.k8$cluster
+adultsCopy.norm.train.labels
+
+#knn=9
+adultsCopy.norm.train.k9<-kmeans(adultsCopy.norm.train, centers=9)
+adultsCopy.norm.train.k9
+adultsCopy.norm.test.k9<-knn(adultsCopy.norm.train, adultsCopy.norm.test, adultsCopy.norm.train.k9$cluster, k=9)
+adultsCopy.norm.test.k9
+adultsCopy.norm.train.labels<-adultsCopy.norm.train.k9$cluster
+adultsCopy.norm.train.labels
+
+#knn=10
+adultsCopy.norm.train.k10<-kmeans(adultsCopy.norm.train, centers=10)
+adultsCopy.norm.train.k10
+adultsCopy.norm.test.k10<-knn(adultsCopy.norm.train, adultsCopy.norm.test, adultsCopy.norm.train.k10$cluster, k=10)
+adultsCopy.norm.test.k10
+adultsCopy.norm.train.labels<-adultsCopy.norm.train.k10$cluster
+adultsCopy.norm.train.labels
+
+#iclust: IDK how to do this and there wasn't any notes on this
+ic.out<-ICLUST(adultsCopy)
+out.file <- file.choose(new=TRUE) 
+ic.out <- ICLUST(adultsCopy,nclusters =10)  #use all defaults and if possible stop at 10 clusters
+ICLUST.graph(ic.out,out.file,title = "IClUST of the data with 10 variable")
 
 
-
-
-
-
-
+#Question 4
 
